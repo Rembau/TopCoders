82214e1aeb8fa9f216e0792ed0931e01aa415230
rembau <504229406@qq.com>
2017-09-20 17:05:54 +0800

    11题 完善

--- src/main/java/projecteuler/P75.java
+++ src/main/java/projecteuler/P75.java
@@ -2,9 +2,16 @@ package projecteuler;
 
 //import java.util.HashMap;
 
-public class P75 {
+import org.junit.Test;
+import projecteuler.tool.Base;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class P75 extends Base {
 /**
-It turns out that 12 cm is the smallest length of wire that can be bent to form an 
+It turns out that 12 cm is the smallest length of wire that can be bent to form an
 integer sided right angle triangle in exactly one way, but there are many more examples.
 12 cm: (3,4,5)
 24 cm: (6,8,10)
@@ -13,30 +20,90 @@ integer sided right angle triangle in exactly one way, but there are many more e
 40 cm: (8,15,17)
 48 cm: (12,16,20)
 
-In contrast, some lengths of wire, like 20 cm, cannot be bent to form an integer sided right angle triangle, 
-and other lengths allow more than one solution to be found; for example, 
+In contrast, some lengths of wire, like 20 cm, cannot be bent to form an integer sided right angle triangle,
+and other lengths allow more than one solution to be found; for example,
 using 120 cm it is possible to form exactly three different integer sided right angle triangles.
 
 120 cm: (30,40,50), (20,48,52), (24,45,51)
 
-Given that L is the length of the wire, for how many values of L <= 1,500,000 
+Given that L is the length of the wire, for how many values of L <= 1,500,000
 can exactly one integer sided right angle triangle be formed?
 */
-	static int n=2000;
-	static int mark[] = new int[n+1];
-	public static void func(){
-		int limit_i=n/3;
-		for(int i=3;i<limit_i;i++){
-			int square_i=i*i;
-			for(int j=i+1;;j++){
+	@Test
+	public void func() {
+        int n=15000;
+        int mark[] = new int[n];
+        Arrays.fill(mark, 0);
 
-			}
-		}
-	}
-	public static void main(String[] args) {
-		long i =System.currentTimeMillis();
-		func();
-		System.out.println(System.currentTimeMillis()-i);
-	}
+        long mark1[] = new long[n];
+        for (long i = 0; i < mark1.length; i++) {
+            mark1[(int)i] = i*i;
+        }
 
+        int tem = 0;
+        int count = 0;
+        List<Integer> list = new LinkedList<>();
+
+        int i1 = n / 3;
+        for (int i = 1; i < i1; i ++) {
+            int i2 = (n - i) / 2;
+            for (int j = i + 1; j < i2; j ++) {
+
+                double sqrt = Math.sqrt(mark1[i] + mark1[j]);
+
+                int b = (int)sqrt;
+                if (sqrt - b == 0) {
+//                    System.out.println(i + " " + j + " " + sqrt + " == " + (i+j+b));
+
+//                    int xtem = mark[i + j + b];
+                    count = count + 1;
+//                    sqrt = Math.sqrt(mark1[i] + mark1[j]);
+
+//                    list.add(i + j + b);
+                }
+
+            }
+        }
+        System.out.println(count);
+    }
+
+    @Test
+    public void f2() {
+	    int n = 1500000;
+
+	    for (long i = 0;i < n;i ++) {
+//            System.out.println(i);
+            for (long j = 0;j < n;j ++) {
+//                for (int k = 0;k < n;k ++) {
+//
+//                }
+                double sqrt = Math.sqrt(i * i + j * j);
+            }
+        }
+    }
+
+    @Test
+    public void f3() {
+        int n = 1500000;
+        int count = 0;
+        for (long i = 0;i < n;i ++) {
+//            System.out.println(i);
+            for (long j = 0;j < n;j ++) {
+//                for (int k = 0;k < n;k ++) {
+//
+//                }
+//                double sqrt = Math.sqrt(i * i + j * j);
+                count ++;
+            }
+        }
+        System.out.println(count);
+    }
+
+    @Test
+    public void test() {
+        double i = 10.5;
+
+        System.out.println(i % 1);
+        System.out.println(i % 1.0);
+    }
 }
--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -2,6 +2,8 @@ package test;
 
 public class Test {
 	public static void main(String args[]){
-		
-	}
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Long.MAX_VALUE);
+        System.out.println(1500000L * 1500000);
+    }
 }
